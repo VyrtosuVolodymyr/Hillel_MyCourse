e8c3a7ae12322a39c43ebb2651c7032534d1d7e8
VyrtosuVolodymyr <vova.virtosu09@gmail.com>
2024-04-17 11:59:57 +0200

    AdJj

---
+++ DZ5/Tablica/TabliCCa/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabliCCa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 10; row++)
+            {
+                for (int col = 1; col <= 10; col++)
+                {
+
+                    Console.Write($"{col} x {row} = {col * row,2}");
+
+
+                    if (col != 10)
+                    {
+                        Console.Write("\t");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
