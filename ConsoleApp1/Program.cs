0b53143d78db037007c3e45c951480830671fd0d
VyrtosuVolodymyr <vova.virtosu09@gmail.com>
2024-04-17 11:47:04 +0200

    addPj

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu();
+            Market market = new Market();
+            Market.ShowMarket(market);
+            Console.WriteLine();
+            bool CheckBuy = true;
+            while (CheckBuy)
+            {
+                Console.Write("-What you want buy?: ");
+                string chooseUser = Console.ReadLine();
+                if (chooseUser == "PS5" || chooseUser == "ps5")
+                {
+                    Console.WriteLine("Nice, you buy PS5");
+                    CheckBuy = false;
+                }
+                else if (chooseUser == "XBOX ONE" || chooseUser == "Xbox one" || chooseUser == "Xbox One" || chooseUser == "xbox one")
+                {
+                    Console.WriteLine("-Nice you but XBOX ONE1");
+                    CheckBuy = false;
+                }
+                else
+                {
+                    Console.WriteLine("-Plese write what you want buy!");
+                }
+            }
+            
+                
+            
+
+        }
+
+        static void Menu() 
+        {
+            Console.WriteLine("================================================================ SHOP FROM GAMERS =================================================================");
+            Console.WriteLine("-App made by vova_rtt");
+
+        }
+
+        public class Market
+        {
+            public string PS5 = "PS5";
+            public int PricePS5 = 550;
+
+            public string XBOXONE = "XBOX ONE";
+            public int PriceXboxOne = 450;
+
+           public static void ShowMarket(Market market) 
+            {
+                Console.WriteLine($"{market.PS5}. Price = {market.PricePS5}$");
+                Console.WriteLine($"{market.XBOXONE}. Proce = {market.PriceXboxOne}$");
+
+            }
+        
+        }
+
+            
+    }
+}
---
+++ DZ5/Root/RootPassword/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RootPassword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string correctPassword = "root";
+            while (true)
+            {
+                Console.Write("-Write correct password: ");
+                string userPass = Console.ReadLine();
+                if (userPass == correctPassword)
+                {
+                    Console.WriteLine("-Good! You write correct password!");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Error!No correct password!");
+
+                }
+            }
+        }
+    }
+}
