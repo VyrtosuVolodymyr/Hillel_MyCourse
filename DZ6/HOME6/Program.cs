8fff4b90735423d6ecc7a55deec2de0a9e102e16
VyrtosuVolodymyr <vova.virtosu09@gmail.com>
2024-04-22 12:53:37 +0200

    First ex.

---
+++ DZ6/HOME6/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOME6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[10];
+            Random rand = new Random();
+            int countPositive = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-100, 101);
+                if (array[i] > 0)
+                {
+                    countPositive++;
+                }
+            }
+
+            Console.Write("Array: ");
+            foreach (int number in array)
+            {
+                Console.Write(number + " ");
+            }
+
+            Console.WriteLine("\nPositive numbers: " + countPositive);
+        }
+    }
+}
