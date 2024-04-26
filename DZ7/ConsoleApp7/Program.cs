0cb60d9cd57931e8dcae9ccdc593755df2977fd0
VyrtosuVolodymyr <vova.virtosu09@gmail.com>
2024-04-26 15:18:07 +0200

    A

--- DZ7/ConsoleApp7/Program.cs
+++ DZ7/ConsoleApp7/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppNumber7
+namespace ConsoleApp7
 {
     internal class Program
     {
--- DZ7/ConsoleApp7/Properties/AssemblyInfo.cs
+++ DZ7/ConsoleApp7/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Ogólne informacje o zestawie są kontrolowane poprzez następujący 
 // zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
 // powiązane z zestawem.
-[assembly: AssemblyTitle("AppNumber7")]
+[assembly: AssemblyTitle("ConsoleApp7")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AppNumber7")]
+[assembly: AssemblyProduct("ConsoleApp7")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Następujący identyfikator GUID jest identyfikatorem biblioteki typów w przypadku udostępnienia tego projektu w modelu COM
-[assembly: Guid("a7e29592-4f4d-4c97-ae26-03db6c6fc356")]
+[assembly: Guid("28592440-839d-41dd-8a17-18a23ea2e9e8")]
 
 // Informacje o wersji zestawu zawierają następujące cztery wartości:
 //
