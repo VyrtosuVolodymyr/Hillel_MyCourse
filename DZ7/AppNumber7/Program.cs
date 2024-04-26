e890decd21b222699eb96a7ff5d7007a7fc1b7fa
VyrtosuVolodymyr <vova.virtosu09@gmail.com>
2024-04-26 15:14:47 +0200

    Add

---
+++ DZ7/AppNumber7/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppNumber7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество предметов:");
+            int subjectsCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] punkts = new int[3];
+            string[] subjects = new string[subjectsCount];
+
+            for (int i = 0; i < subjectsCount; i++)
+            {
+                Console.Write($"Введите название предмета {i + 1}: ");
+                subjects[i] = Console.ReadLine();
+                Console.Write($"Введите оценку по предмету {subjects[i]}: ");
+                punkts[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            double average = Average(punkts);
+
+            Console.WriteLine("Оценки студента: ");
+            for (int i = 0; i < subjectsCount; i++)
+            {
+                Console.WriteLine($"{subjects[i]}: {punkts[i]}");
+            }
+            Console.WriteLine($"Средняя оценка:{average} ");
+        }
+
+        static double Average(int[] punkts)
+        {
+            int sum = 0;
+            foreach (int i in punkts)
+            {
+                sum += i;
+            }
+
+            return (double)sum / punkts.Length;
+        }
+
+    }
+}
