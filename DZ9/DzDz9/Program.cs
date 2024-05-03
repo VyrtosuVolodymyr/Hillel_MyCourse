764ab818c10453b6178bff578dca77748650099f
VyrtosuVolodymyr <vova.virtosu09@gmail.com>
2024-05-03 12:48:26 +0200

    doz

---
+++ DZ9/DzDz9/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DzDz9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Paradise", "Djoseph Lori", 2005, 650);
+            book1.DisplayInfo();
+            book1.IsThik();
+
+            Book book2 = new Book("Mini-Lord", "Rozed Inn", 2021, 450);
+            book2.DisplayInfo();
+            book2.IsThik();
+
+
+        }
+
+        public class Book
+        {
+            public string nameBook { get; set; }
+            public string autorBook { get; set; }
+            public int ageBook { get; set; }
+            public int pagesBook { get; set; }
+
+            public Book(string nameBook, string autorBook, int ageBook, int pagesBook)
+            {
+                this.nameBook = nameBook;
+                this.autorBook = autorBook;
+                this.ageBook = ageBook;
+                this.pagesBook = pagesBook;
+            }
+
+            public void DisplayInfo()
+            {
+                Console.WriteLine($"Name of book:{nameBook} \nAutor:{autorBook} \nYear:{ageBook} \nPages:{pagesBook}");
+            }
+
+            public void IsThik()
+            {
+                if (pagesBook > 500)
+                {
+                    Console.WriteLine("It's big book!");
+                }
+                else
+                {
+                    Console.WriteLine("It's not big book!");
+                }
+            }
+        }
+
+
+    }
+}
